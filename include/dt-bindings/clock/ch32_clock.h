29d09f2a04b010158081deaf1f0810f822002557
zerosensei <1054536335@qq.com>
2022-03-30 16:41:52 +0800

    Add board: CH32V307_VCT6

---
+++ include/dt-bindings/clock/ch32_clock.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2021 Nanjing Qinheng Microelectronics Co., Ltd.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_CH32_CLOCK_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_CH32_CLOCK_H_
+
+/* clock bus references */
+#define CH32_CLOCK_BUS_AHB      0
+#define CH32_CLOCK_BUS_APB1     1
+#define CH32_CLOCK_BUS_APB2     2
+
+#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_CH32_CLOCK_H_ */
