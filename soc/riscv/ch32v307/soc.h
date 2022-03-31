bd1b4e90e3f892097c433f2cd0f5f03753ec7d59
zerosensei <1054536335@qq.com>
2022-03-31 16:43:15 +0800

    soc and board

---
+++ soc/riscv/ch32v307/soc.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2021 Nanjing Qinheng Microelectronics
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _CH32V307_SOC_H_
+#define _CH32V307_SOC_H_
+
+#ifndef _ASMLANGUAGE
+
+#include <stm32f1xx.h>
+
+/* Add generated devicetree information and STM32 helper macros */
+#include <st_stm32_dt.h>
+
+#endif /* !_ASMLANGUAGE */
+
+#endif /* _CH32V307_SOC_H_ */
