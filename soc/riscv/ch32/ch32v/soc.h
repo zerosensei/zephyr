46e26867cbf4c97fa975db43398638b3bcd3fbd2
zerosensei <1054536335@qq.com>
2022-04-15 17:28:26 +0800

    add modules

--- soc/riscv/ch32/ch32v/soc.h
+++ soc/riscv/ch32/ch32v/soc.h
@@ -7,13 +7,21 @@
 #ifndef _CH32V30x_SOC_H_
 #define _CH32V30x_SOC_H_
 
-#ifndef _ASMLANGUAGE
 
 #include <ch32v30x.h>
+#include "ch32v30x_common.h"
 
-/* Add generated devicetree information and STM32 helper macros */
-// #include <st_stm32_dt.h>
 
-#endif /* !_ASMLANGUAGE */
+/* ECALL Exception numbers */
+#define SOC_MCAUSE_ECALL_EXP 5 /* Machine ECALL instruction */
+#define SOC_MCAUSE_USER_ECALL_EXP 8 /* User ECALL instruction */
+
+/* Interrupt Mask */
+#define SOC_MCAUSE_IRQ_MASK (1 << 31)
+/* Exception code Mask */
+#define SOC_MCAUSE_EXP_MASK 0x7FFFFFFF
+
+/* SOC-Specific EXIT ISR command */
+#define SOC_ERET mret
 
 #endif /* _CH32V307_SOC_H_ */
