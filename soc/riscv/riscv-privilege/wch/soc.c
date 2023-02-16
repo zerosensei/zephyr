179ddb3018c0a29794ca59fa4031ede809310503
zerosensei <1054536335@qq.com>
2023-02-16 17:59:23 +0800

    add soc

---
+++ soc/riscv/riscv-privilege/wch/soc.c
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2023 zerosensei
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/init.h>
+
+
+
+static int ch58x_init(const struct device *arg)
+{
+    
+}
---
+++ soc/riscv/riscv-privilege/wch/soc.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2023 zerosensei
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef SOC_RISCV_RISCV_PRIVILEGE_WCH_SOC_H
+#define SOC_RISCV_RISCV_PRIVILEGE_WCH_SOC_H
+
+
+
+#endif /* SOC_RISCV_RISCV_PRIVILEGE_WCH_SOC_H */
